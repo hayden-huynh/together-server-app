bbb7b4bc3f1d7e43cfaef17a3aa17bf3a1c11770
Hayden Huynh <huynhhai323@gmail.com>
2021-10-14 20:47:38 +0700

    Removed public and view engine

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ const { startupSchedule } = require("./utilities/scheduleCloudMessaging");
 
 const app = express();
 
-app.set("view engine", "ejs");
-
-app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json({ extended: true }));
