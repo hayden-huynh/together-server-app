9022ad04ec3fe7e743916026ab450242292e6063
Hayden Huynh <huynhhai323@gmail.com>
2021-10-19 22:28:43 +0700

    Updated database name in registerAdmin to check-in-data

--- utilities/registerAdmin.js
+++ utilities/registerAdmin.js
@@ -10,7 +10,7 @@ const hashPassword = async (password) => {
 };
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/dev", {
+  .connect("mongodb://127.0.0.1:27017/check-in-data", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
